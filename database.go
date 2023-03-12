c84a031d163ce86c790af32a15a581583645366f
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-12 17:12:53 +0700

    membuat function connection

---
+++ database.go
@@ -0,0 +1,21 @@
+package belajar_golang_database
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar-golang-database")
+	if err != nil {
+		panic(err)
+	}
+
+	// wajib setting connection poolingnya agar optimal
+	db.SetConnMaxIdleTime(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
