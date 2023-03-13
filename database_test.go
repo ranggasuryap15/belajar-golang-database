c56c56d13a14dd8174d6777fc865dfbd20afb938
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-13 13:34:23 +0700

    query sql complex

--- database_test.go
+++ database_test.go
@@ -1,9 +1,13 @@
 package belajar_golang_database
 
 import (
+	"context"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"fmt"
 	"testing"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestEmpty(t *testing.T) {
@@ -19,5 +23,39 @@ func TestOpenConnection(t *testing.T) {
 	err = db.Close()
 	if err != nil {
 		return
+	}
+}
+
+func TestQuerySQLComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name, email string
+		var balance int32
+		var rating float64
+		var birthDate, createdAt time.Time
+		var married bool
+
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("ID:", id, "Name:", name, "Email", email, "balance:", balance, "rating:", rating, "birth date:", birthDate, "married:", married, "creted at:", createdAt)
+		fmt.Println("============")
+
 	}
 }
